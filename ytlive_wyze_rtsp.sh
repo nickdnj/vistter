29b1f8ffb0806fd164b1de6f9704c6c8f5545989
nickdnj <nickd@demarconet.com>
2021-12-23 17:50:46 -0500

    Update ytlive_wyze_rtsp.sh

--- ytlive_wyze_rtsp.sh
+++ ytlive_wyze_rtsp.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 valid=true
 count=1
+YOUTUBE_URL="rtmp://x.rtmp.youtube.com/live2/YOUTUBE_LIVE_KEY"
 
 while [ $valid ]
 do
 
-ffmpeg -thread_queue_size 2048 -t 00:30:00 -i "rtsp://Condo146:Wharfside@192.168.86.222/live" -f lavfi -i anullsrc -v
-codec copy -acodec aac -shortest  -f flv rtmp://a.rtmp.youtube.com/live2/
+ffmpeg -thread_queue_size 2048 -t 00:30:00 -i "rtsp://<username>:<password>192.168.86.222/live" -f lavfi -i anullsrc -v
+codec copy -acodec aac -shortest  -f flv $YOUTUBE_URL
 
 echo $count >> /tmp/streamcnt.txt
 
